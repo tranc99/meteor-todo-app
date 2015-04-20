1ad3502a7cc4ebbf2befd359aad18b8346aa3fa3
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-20 23:02:42 +0000

    far from the madding crowd

---
+++ todos.js
@@ -0,0 +1,49 @@
+Todos = new Mongo.Collection("todos")
+
+function Todo(name) {
+    this.name = name;
+}
+
+Todo.prototype = {
+    valid: function() {
+        return this.name && this.name != "";
+    },
+    save: function() {
+        Todos.insert({name: this.name});
+    }
+};
+
+function prefillTodos() {
+    if(Todos.find().count !== 0) return;
+
+    var todos = ["Create a Todo", "Learn Cowboy Erlang"];
+
+    todos.forEach(function(todo) { new Todo(todo).save(); });
+}
+
+if (Meteor.isServer) Meteor.startup(prefillTodos);
+
+function setUpTodoTemplates() {
+    Template.todos.helpers({
+        todos: function () {
+            return Todos.find();
+        }
+    });
+
+    Template.createTodo.events({
+        'click button': function() {
+            var input = $("#newTodo");
+            var name = input.val();
+
+            input.val("");
+
+            new Todo(name).save();
+        }
+    })
+}
+
+if (Meteor.isClient) {
+
+    setUpTodoTemplates();
+
+}
